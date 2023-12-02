0f5c5e456f7e7d08b82331fc8608292b2ecbd783
Ty Everett <ty@tyweb.us>
2023-12-01 19:06:22 -0800

    Failing P2PKH vector

--- src/script/__tests/Spend.test.ts
+++ src/script/__tests/Spend.test.ts
@@ -41,4 +41,38 @@ describe('Spend', () => {
     const valid = spend.validate()
     expect(valid).toBe(true)
   })
+  it('Fails to verify a P2PKH spend with the wrong key', () => {
+    const privateKey = new PrivateKey(1)
+    const publicKey = new Curve().g.mul(privateKey)
+    const wrongPrivateKey = new PrivateKey(2)
+    const hash = hash160(publicKey.encode(true)) as number[]
+    const p2pkh = new P2PKH()
+    const lockingScript = p2pkh.lock(hash)
+    const satoshis = new BigNumber(1)
+    const unlockingTemplate = p2pkh.unlock(wrongPrivateKey)
+    const sourceTx = new Transaction(1, [], [{
+      lockingScript,
+      satoshis
+    }], 0)
+    const spendTx = new Transaction(1, [{
+      sourceTransaction: sourceTx,
+      sourceOutputIndex: 0,
+      sequence: 0xffffffff
+    }], [], 0)
+    const unlockingScript = unlockingTemplate(spendTx, 0)
+    const spend = new Spend({
+      sourceTXID: sourceTx.id('hex'),
+      sourceOutputIndex: 0,
+      sourceSatoshis: satoshis,
+      lockingScript,
+      transactionVersion: 1,
+      otherInputs: [],
+      inputIndex: 0,
+      unlockingScript,
+      outputs: [],
+      inputSequence: 0xffffffff,
+      lockTime: 0
+    })
+    expect(() => spend.validate()).toThrow()
+  })
 })
