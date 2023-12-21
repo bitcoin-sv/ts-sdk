b4fd8a446dde36000e7d46b5d6ddd95f87e68c32
Ty Everett <ty@tyweb.us>
2023-12-21 11:40:26 -0800

    Sad-case testing

--- src/messages/__tests/EncryptedMessage.test.ts
+++ src/messages/__tests/EncryptedMessage.test.ts
@@ -11,4 +11,26 @@ describe('EncryptedMessage', () => {
     const decrypted = decrypt(encrypted, recipient)
     expect(decrypted).toEqual(message)
   })
+  it('Fails to decrypt a message with wrong version', () => {
+    const sender = new PrivateKey(15)
+    const recipient = new PrivateKey(21)
+    const recipientPub = recipient.toPublicKey()
+    const message = [1, 2, 4, 8, 16, 32]
+    const encrypted = encrypt(message, sender, recipientPub)
+    encrypted[0] = 1
+    expect(() => decrypt(encrypted, recipient)).toThrow(new Error(
+      'Message version mismatch: Expected 42421033, received 01421033'
+    ))
+  })
+  it('Fails to decrypt a message with wrong recipient', () => {
+    const sender = new PrivateKey(15)
+    const recipient = new PrivateKey(21)
+    const wrongRecipient = new PrivateKey(22)
+    const recipientPub = recipient.toPublicKey()
+    const message = [1, 2, 4, 8, 16, 32]
+    const encrypted = encrypt(message, sender, recipientPub)
+    expect(() => decrypt(encrypted, wrongRecipient)).toThrow(new Error(
+      'The encrypted message expects a recipient public key of 02352bbf4a4cdd12564f93fa332ce333301d9ad40271f8107181340aef25be59d5, but the provided key is 03421f5fc9a21065445c96fdb91c0c1e2f2431741c72713b4b99ddcb316f31e9fc'
+    ))
+  })
 })
--- src/messages/__tests/SignedMessage.test.ts
+++ src/messages/__tests/SignedMessage.test.ts
@@ -18,4 +18,36 @@ describe('SignedMessage', () => {
     const verified = verify(message, signature)
     expect(verified).toEqual(true)
   })
+  it('Fails to verify a message with a wrong version', () => {
+    const sender = new PrivateKey(15)
+    const recipient = new PrivateKey(21)
+    const recipientPub = recipient.toPublicKey()
+    const message = [1, 2, 4, 8, 16, 32]
+    const signature = sign(message, sender, recipientPub)
+    signature[0] = 1
+    expect(() => verify(message, signature, recipient)).toThrow(new Error(
+      'Message version mismatch: Expected 42423301, received 01423301'
+    ))
+  })
+  it('Fails to verify a message with no verifier when required', () => {
+    const sender = new PrivateKey(15)
+    const recipient = new PrivateKey(21)
+    const recipientPub = recipient.toPublicKey()
+    const message = [1, 2, 4, 8, 16, 32]
+    const signature = sign(message, sender, recipientPub)
+    expect(() => verify(message, signature)).toThrow(new Error(
+      'This signature can only be verified with knowledge of a specific private key. The associated public key is: 02352bbf4a4cdd12564f93fa332ce333301d9ad40271f8107181340aef25be59d5'
+    ))
+  })
+  it('Fails to verify a message with a wrong verifier', () => {
+    const sender = new PrivateKey(15)
+    const recipient = new PrivateKey(21)
+    const wrongRecipient = new PrivateKey(22)
+    const recipientPub = recipient.toPublicKey()
+    const message = [1, 2, 4, 8, 16, 32]
+    const signature = sign(message, sender, recipientPub)
+    expect(() => verify(message, signature, wrongRecipient)).toThrow(new Error(
+      'The recipient public key is 03421f5fc9a21065445c96fdb91c0c1e2f2431741c72713b4b99ddcb316f31e9fc but the signature requres the recipient to have public key 02352bbf4a4cdd12564f93fa332ce333301d9ad40271f8107181340aef25be59d5'
+    ))
+  })
 })
