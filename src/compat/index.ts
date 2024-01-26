032d1560295d7a83fcfad222706e901ff090b3c6
Darren Kellenschwiler <d.kellenschwiler@bsvblockchain.org>
2024-01-26 13:46:41 -0600

    [fix] export for HD class should also be named
    Signed-off-by: Darren Kellenschwiler <d.kellenschwiler@bsvblockchain.org>

--- src/compat/index.ts
+++ src/compat/index.ts
@@ -1,4 +1,4 @@
 export * as BSM from './BSM.js'
-export * as HD from './HD.js'
+export { default as HD } from './HD.js'
 export { default as Mnemonic } from './Mnemonic.js'
 export * as ECIES from './ECIES.js'
