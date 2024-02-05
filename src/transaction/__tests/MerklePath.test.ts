2263500d26288c97a57e7f2a586d024ef2a0e968
Darren Kellenschwiler <d.kellenschwiler@bsvblockchain.org>
2024-02-04 21:59:04 -0600

    [fix] - additional checking for extra or invalid data inclusion within BUMP format
    Additional checking as fleshed out in the BUMP feedback discussion [here](https://github.com/bitcoin-sv/BRCs/issues/67).
    
    Signed-off-by: Darren Kellenschwiler <d.kellenschwiler@bsvblockchain.org>

--- src/transaction/__tests/MerklePath.test.ts
+++ src/transaction/__tests/MerklePath.test.ts
@@ -1,4 +1,6 @@
 import MerklePath from '../../../dist/cjs/src/transaction/MerklePath'
+import invalidBumps from './bump.invalid.vectors'
+import validBumps from './bump.valid.vectors'
 
 const BRC74Hex = 'fe8a6a0c000c04fde80b0011774f01d26412f0d16ea3f0447be0b5ebec67b0782e321a7a01cbdf7f734e30fde90b02004e53753e3fe4667073063a17987292cfdea278824e9888e52180581d7188d8fdea0b025e441996fc53f0191d649e68a200e752fb5f39e0d5617083408fa179ddc5c998fdeb0b0102fdf405000671394f72237d08a4277f4435e5b6edf7adc272f25effef27cdfe805ce71a81fdf50500262bccabec6c4af3ed00cc7a7414edea9c5efa92fb8623dd6160a001450a528201fdfb020101fd7c010093b3efca9b77ddec914f8effac691ecb54e2c81d0ab81cbc4c4b93befe418e8501bf01015e005881826eb6973c54003a02118fe270f03d46d02681c8bc71cd44c613e86302f8012e00e07a2bb8bb75e5accff266022e1e5e6e7b4d6d943a04faadcf2ab4a22f796ff30116008120cafa17309c0bb0e0ffce835286b3a2dcae48e4497ae2d2b7ced4f051507d010a00502e59ac92f46543c23006bff855d96f5e648043f0fb87a7a5949e6a9bebae430104001ccd9f8f64f4d0489b30cc815351cf425e0e78ad79a589350e4341ac165dbe45010301010000af8764ce7e1cc132ab5ed2229a005c87201c9a5ee15c0f91dd53eff31ab30cd4'
 
@@ -130,15 +132,21 @@ describe('MerklePath', () => {
   it('Combines two paths', () => {
     const path0A = [...BRC74JSON.path[0]]
     const path0B = [...BRC74JSON.path[0]]
+    const path1A = [...BRC74JSON.path[1]]
+    const path1B = [...BRC74JSON.path[1]]
     const pathRest = [...BRC74JSON.path]
+    pathRest.shift()
     pathRest.shift()
     path0A.splice(2, 2)
     path0B.shift()
     path0B.shift()
+    path1A.shift()
+    path1B.pop()
     const pathAJSON = {
       blockHeight: BRC74JSON.blockHeight,
       path: [
         path0A,
+        path1A,
         ...pathRest
       ]
     }
@@ -146,6 +154,7 @@ describe('MerklePath', () => {
       blockHeight: BRC74JSON.blockHeight,
       path: [
         path0B,
+        path1B,
         ...pathRest
       ]
     }
@@ -158,5 +167,15 @@ describe('MerklePath', () => {
     pathA.combine(pathB)
     expect(pathA.computeRoot(BRC74TXID2)).toEqual(BRC74Root)
     expect(pathA.computeRoot(BRC74TXID3)).toEqual(BRC74Root)
+  })
+  it('Rejects invalid bumps', () => {
+    for (const invalid of invalidBumps) {
+      expect(() => MerklePath.fromHex(invalid.bump)).toThrow(invalid.error)
+    }
+  })
+  it('Verifies valid bumps', async () => {
+    for (const valid of validBumps) {
+      expect(() => MerklePath.fromHex(valid.bump)).not.toThrowError()
+    }
   })
 })
