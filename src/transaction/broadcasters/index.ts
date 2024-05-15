ecdfc069c2301963c00ff717a797ecd925e78574
Damian Orzepowski <damian.orzepowski@4chain.studio>
2024-05-15 10:16:00 +0200

    feat: expose HttpClient on index.ts of ARC

--- src/transaction/broadcasters/index.ts
+++ src/transaction/broadcasters/index.ts
@@ -1,1 +1,5 @@
 export { default as ARC } from './ARC.js'
+export type { HttpClient, HttpClientResponse, HttpClientRequestOptions } from './HttpClient.js'
+export { default as defaultHttpClient } from './DefaultHttpClient.js'
+export { NodejsHttpClient } from './NodejsHttpClient.js'
+export type { HttpsNodejs, NodejsHttpClientRequest } from './NodejsHttpClient.js'
