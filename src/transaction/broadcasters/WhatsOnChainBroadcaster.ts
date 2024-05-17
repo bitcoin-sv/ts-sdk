46a0bd6554c13ce9f32ef1533d046fa8e8487ce1
Damian Orzepowski <damian.orzepowski@4chain.studio>
2024-05-17 15:54:55 +0200

    feat: add tests for WhatsOnChainBroadcaster

--- src/transaction/broadcasters/WhatsOnChainBroadcaster.ts
+++ src/transaction/broadcasters/WhatsOnChainBroadcaster.ts
@@ -7,64 +7,64 @@ import {defaultHttpClient} from "../http/DefaultHttpClient.js";
  * Represents an WhatsOnChain transaction broadcaster.
  */
 export default class WhatsOnChainBroadcaster implements Broadcaster {
-    network: string
-    URL: string
-    private httpClient: HttpClient;
+  network: string
+  URL: string
+  private httpClient: HttpClient;
 
-    /**
-     * Constructs an instance of the WhatsOnChain broadcaster.
-     *
-     * @param {string} network - The URL endpoint for the WhatsOnChain API.
-     * @param {HttpClient} httpClient - The HTTP client used to make requests to the ARC API.
-     */
-    constructor(network: string = 'main', httpClient: HttpClient = defaultHttpClient()) {
-        this.network = network
-        this.URL = `https://api.whatsonchain.com/v1/bsv/${network}/tx/raw`
-        this.httpClient = httpClient
+  /**
+   * Constructs an instance of the WhatsOnChain broadcaster.
+   *
+   * @param {string} network - The URL endpoint for the WhatsOnChain API.
+   * @param {HttpClient} httpClient - The HTTP client used to make requests to the ARC API.
+   */
+  constructor(network: string = 'main', httpClient: HttpClient = defaultHttpClient()) {
+    this.network = network
+    this.URL = `https://api.whatsonchain.com/v1/bsv/${network}/tx/raw`
+    this.httpClient = httpClient
+  }
+
+  /**
+   * Broadcasts a transaction via WhatsOnChain.
+   *
+   * @param {Transaction} tx - The transaction to be broadcasted.
+   * @returns {Promise<BroadcastResponse | BroadcastFailure>} A promise that resolves to either a success or failure response.
+   */
+  async broadcast(tx: Transaction): Promise<BroadcastResponse | BroadcastFailure> {
+    let rawTx = tx.toHex()
+
+    const requestOptions = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'text/plain'
+      },
+      data: {txhex: rawTx}
     }
 
-    /**
-     * Broadcasts a transaction via WhatsOnChain.
-     *
-     * @param {Transaction} tx - The transaction to be broadcasted.
-     * @returns {Promise<BroadcastResponse | BroadcastFailure>} A promise that resolves to either a success or failure response.
-     */
-    async broadcast(tx: Transaction): Promise<BroadcastResponse | BroadcastFailure> {
-        let rawTx = tx.toHex()
-
-        const requestOptions = {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'text/plain'
-            },
-            data: {txhex: rawTx}
+    try {
+      const response = await this.httpClient.request<string>(this.URL, requestOptions)
+      if (response.ok) {
+        const txid = response.data
+        return {
+          status: 'success',
+          txid: txid,
+          message: 'broadcast successful'
         }
-
-        try {
-            const response = await this.httpClient.request<string>(this.URL, requestOptions)
-            if (response.ok) {
-                const txid = response.data
-                return {
-                    status: 'success',
-                    txid: txid,
-                    message: 'broadcast success'
-                }
-            } else {
-                return {
-                    status: 'error',
-                    code: response.statusText ?? 'ERR_UNKNOWN',
-                    description: response.data?.detail ?? 'Unknown error'
-                }
-            }
-        } catch (error) {
-            return {
-                status: 'error',
-                code: '500',
-                description: typeof error.message === 'string'
-                    ? error.message
-                    : 'Internal Server Error'
-            }
+      } else {
+        return {
+          status: 'error',
+          code: response.status.toString() ?? 'ERR_UNKNOWN',
+          description: response.data ?? 'Unknown error'
         }
+      }
+    } catch (error) {
+      return {
+        status: 'error',
+        code: '500',
+        description: typeof error.message === 'string'
+          ? error.message
+          : 'Internal Server Error'
+      }
     }
+  }
 }
