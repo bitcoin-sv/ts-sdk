5609fad519e217d053c8a98bd8c48f8dfd74fa65
Ty J Everett <ty@tyweb.us>
2024-10-06 19:51:56 -0700

    Lint

--- src/primitives/TransactionSignature.ts
+++ src/primitives/TransactionSignature.ts
@@ -15,7 +15,7 @@ export default class TransactionSignature extends Signature {
 
   scope: number
 
-  static format(params: {
+  static format (params: {
     sourceTXID: string
     sourceOutputIndex: number
     sourceSatoshis: number
@@ -64,7 +64,7 @@ export default class TransactionSignature extends Signature {
       return ret
     }
 
-    function getOutputsHash(outputIndex?: number): number[] {
+    function getOutputsHash (outputIndex?: number): number[] {
       const writer = new Writer()
 
       if (typeof outputIndex === 'undefined') {
@@ -147,7 +147,7 @@ export default class TransactionSignature extends Signature {
   }
 
   // The format used in a tx
-  static fromChecksigFormat(buf: number[]): TransactionSignature {
+  static fromChecksigFormat (buf: number[]): TransactionSignature {
     if (buf.length === 0) {
       // allow setting a "blank" signature
       const r = new BigNumber(1)
@@ -161,7 +161,7 @@ export default class TransactionSignature extends Signature {
     return new TransactionSignature(tempSig.r, tempSig.s, scope)
   }
 
-  constructor(r: BigNumber, s: BigNumber, scope: number) {
+  constructor (r: BigNumber, s: BigNumber, scope: number) {
     super(r, s)
     this.scope = scope
   }
@@ -171,7 +171,7 @@ export default class TransactionSignature extends Signature {
      * See also Ecdsa signature algorithm which enforces this.
      * See also Bip 62, "low S values in signatures"
      */
-  public hasLowS(): boolean {
+  public hasLowS (): boolean {
     if (
       this.s.ltn(1) ||
       this.s.gt(new BigNumber(
@@ -183,7 +183,7 @@ export default class TransactionSignature extends Signature {
     return true
   }
 
-  toChecksigFormat(): number[] {
+  toChecksigFormat (): number[] {
     const derbuf = this.toDER() as number[]
     return [...derbuf, this.scope]
   }
