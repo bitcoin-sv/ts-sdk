1bbff9ab134fedeb573c1f692dea9bcf8e44b006
Darren Kellenschwiler <d.kellenschwiler@bsvblockchain.org>
2024-11-25 08:58:12 -0600

    remove redundant repetative code block
    Signed-off-by: Darren Kellenschwiler <d.kellenschwiler@bsvblockchain.org>

--- src/wallet/WalletClient.ts
+++ src/wallet/WalletClient.ts
@@ -61,198 +61,141 @@ export default class WalletClient implements Wallet {
   }
 
   async createAction(args: CreateActionArgs): Promise<CreateActionResult> {
-    if (typeof this.substrate !== 'object') {
-      await this.connectToSubstrate()
-    }
+    await this.connectToSubstrate()
     return await (this.substrate as Wallet).createAction(args, this.originator)
   }
 
   async signAction(args: SignActionArgs): Promise<SignActionResult> {
-    if (typeof this.substrate !== 'object') {
-      await this.connectToSubstrate()
-    }
+    await this.connectToSubstrate()
     return await (this.substrate as Wallet).signAction(args, this.originator)
   }
 
   async abortAction(args: { reference: Base64String }): Promise<{ aborted: true }> {
-    if (typeof this.substrate !== 'object') {
-      await this.connectToSubstrate()
-    }
+    await this.connectToSubstrate()
     return await (this.substrate as Wallet).abortAction(args, this.originator)
   }
 
   async listActions(args: ListActionsArgs): Promise<ListActionsResult> {
-    if (typeof this.substrate !== 'object') {
-      await this.connectToSubstrate()
-    }
+    await this.connectToSubstrate()
     return await (this.substrate as Wallet).listActions(args, this.originator)
   }
 
   async internalizeAction(args: InternalizeActionArgs): Promise<{ accepted: true }> {
-    if (typeof this.substrate !== 'object') {
-      await this.connectToSubstrate()
-    }
+    await this.connectToSubstrate()
     return await (this.substrate as Wallet).internalizeAction(args, this.originator)
   }
 
   async listOutputs(args: ListOutputsArgs): Promise<ListOutputsResult> {
-    if (typeof this.substrate !== 'object') {
-      await this.connectToSubstrate()
-    }
+    await this.connectToSubstrate()
     return await (this.substrate as Wallet).listOutputs(args, this.originator)
   }
 
   async relinquishOutput(args: { basket: BasketStringUnder300Bytes, output: OutpointString }): Promise<{ relinquished: true }> {
-    if (typeof this.substrate !== 'object') {
-      await this.connectToSubstrate()
-    }
+    await this.connectToSubstrate()
     return await (this.substrate as Wallet).relinquishOutput(args, this.originator)
   }
 
   async getPublicKey(args: { identityKey?: true, protocolID?: [0 | 1 | 2, ProtocolString5To400Bytes], keyID?: KeyIDStringUnder800Bytes, privileged?: BooleanDefaultFalse, privilegedReason?: DescriptionString5to50Bytes, counterparty?: PubKeyHex | 'self' | 'anyone', forSelf?: BooleanDefaultFalse }): Promise<{ publicKey: PubKeyHex }> {
-    if (typeof this.substrate !== 'object') {
-      await this.connectToSubstrate()
-    }
+    await this.connectToSubstrate()
     return await (this.substrate as Wallet).getPublicKey(args, this.originator)
   }
 
   async revealCounterpartyKeyLinkage(args: { counterparty: PubKeyHex, verifier: PubKeyHex, privilegedReason?: DescriptionString5to50Bytes, privileged?: BooleanDefaultFalse }): Promise<{ prover: PubKeyHex, verifier: PubKeyHex, counterparty: PubKeyHex, revelationTime: ISOTimestampString, encryptedLinkage: Byte[], encryptedLinkageProof: Byte[] }> {
-    if (typeof this.substrate !== 'object') {
-      await this.connectToSubstrate()
-    }
+    await this.connectToSubstrate()
     return await (this.substrate as Wallet).revealCounterpartyKeyLinkage(args, this.originator)
   }
 
   async revealSpecificKeyLinkage(args: { counterparty: PubKeyHex, verifier: PubKeyHex, protocolID: [0 | 1 | 2, ProtocolString5To400Bytes], keyID: KeyIDStringUnder800Bytes, privilegedReason?: DescriptionString5to50Bytes, privileged?: BooleanDefaultFalse }): Promise<{ prover: PubKeyHex, verifier: PubKeyHex, counterparty: PubKeyHex, protocolID: [0 | 1 | 2, ProtocolString5To400Bytes], keyID: KeyIDStringUnder800Bytes, encryptedLinkage: Byte[], encryptedLinkageProof: Byte[], proofType: Byte }> {
-    if (typeof this.substrate !== 'object') {
-      await this.connectToSubstrate()
-    }
+    await this.connectToSubstrate()
     return await (this.substrate as Wallet).revealSpecificKeyLinkage(args, this.originator)
   }
 
   async encrypt(args: { plaintext: Byte[], protocolID: [0 | 1 | 2, ProtocolString5To400Bytes], keyID: KeyIDStringUnder800Bytes, privilegedReason?: DescriptionString5to50Bytes, counterparty?: PubKeyHex | 'self' | 'anyone', privileged?: BooleanDefaultFalse }): Promise<{ ciphertext: Byte[] }> {
-    if (typeof this.substrate !== 'object') {
-      await this.connectToSubstrate()
-    }
+    await this.connectToSubstrate()
     return await (this.substrate as Wallet).encrypt(args, this.originator)
   }
 
   async decrypt(args: { ciphertext: Byte[], protocolID: [0 | 1 | 2, ProtocolString5To400Bytes], keyID: KeyIDStringUnder800Bytes, privilegedReason?: DescriptionString5to50Bytes, counterparty?: PubKeyHex | 'self' | 'anyone', privileged?: BooleanDefaultFalse }): Promise<{ plaintext: Byte[] }> {
-    if (typeof this.substrate !== 'object') {
-      await this.connectToSubstrate()
-    }
     return await (this.substrate as Wallet).decrypt(args, this.originator)
   }
 
   async createHmac(args: { data: Byte[], protocolID: [0 | 1 | 2, ProtocolString5To400Bytes], keyID: KeyIDStringUnder800Bytes, privilegedReason?: DescriptionString5to50Bytes, counterparty?: PubKeyHex | 'self' | 'anyone', privileged?: BooleanDefaultFalse }): Promise<{ hmac: Byte[] }> {
-    if (typeof this.substrate !== 'object') {
-      await this.connectToSubstrate()
-    }
+    await this.connectToSubstrate()
     return await (this.substrate as Wallet).createHmac(args, this.originator)
   }
 
   async verifyHmac(args: { data: Byte[], hmac: Byte[], protocolID: [0 | 1 | 2, ProtocolString5To400Bytes], keyID: KeyIDStringUnder800Bytes, privilegedReason?: DescriptionString5to50Bytes, counterparty?: PubKeyHex | 'self' | 'anyone', privileged?: BooleanDefaultFalse }): Promise<{ valid: true }> {
-    if (typeof this.substrate !== 'object') {
-      await this.connectToSubstrate()
-    }
+    await this.connectToSubstrate()
     return await (this.substrate as Wallet).verifyHmac(args, this.originator)
   }
 
   async createSignature(args: { data?: Byte[], hashToDirectlySign?: Byte[], protocolID: [0 | 1 | 2, ProtocolString5To400Bytes], keyID: KeyIDStringUnder800Bytes, privilegedReason?: DescriptionString5to50Bytes, counterparty?: PubKeyHex | 'self' | 'anyone', privileged?: BooleanDefaultFalse }): Promise<{ signature: Byte[] }> {
-    if (typeof this.substrate !== 'object') {
-      await this.connectToSubstrate()
-    }
+    await this.connectToSubstrate()
     return await (this.substrate as Wallet).createSignature(args, this.originator)
   }
 
   async verifySignature(args: { data?: Byte[], hashToDirectlyVerify?: Byte[], signature: Byte[], protocolID: [0 | 1 | 2, ProtocolString5To400Bytes], keyID: KeyIDStringUnder800Bytes, privilegedReason?: DescriptionString5to50Bytes, counterparty?: PubKeyHex | 'self' | 'anyone', forSelf?: BooleanDefaultFalse, privileged?: BooleanDefaultFalse }): Promise<{ valid: true }> {
-    if (typeof this.substrate !== 'object') {
-      await this.connectToSubstrate()
-    }
+    await this.connectToSubstrate()
     return await (this.substrate as Wallet).verifySignature(args, this.originator)
   }
 
   async acquireCertificate(args: AcquireCertificateArgs): Promise<AcquireCertificateResult> {
-    if (typeof this.substrate !== 'object') {
-      await this.connectToSubstrate()
-    }
+    await this.connectToSubstrate()
     return await (this.substrate as Wallet).acquireCertificate(args, this.originator)
   }
 
   async listCertificates(args: { certifiers: PubKeyHex[], types: Base64String[], limit?: PositiveIntegerDefault10Max10000, offset?: PositiveIntegerOrZero, privileged?: BooleanDefaultFalse, privilegedReason?: DescriptionString5to50Bytes }): Promise<ListCertificatesResult> {
-    if (typeof this.substrate !== 'object') {
-      await this.connectToSubstrate()
-    }
+    await this.connectToSubstrate()
     return await (this.substrate as Wallet).listCertificates(args, this.originator)
   }
 
   async proveCertificate(args: ProveCertificateArgs): Promise<ProveCertificateResult> {
-    if (typeof this.substrate !== 'object') {
-      await this.connectToSubstrate()
-    }
+    await this.connectToSubstrate()
     return await (this.substrate as Wallet).proveCertificate(args, this.originator)
   }
 
   async relinquishCertificate(args: { type: Base64String, serialNumber: Base64String, certifier: PubKeyHex }): Promise<{ relinquished: true }> {
-    if (typeof this.substrate !== 'object') {
-      await this.connectToSubstrate()
-    }
+    await this.connectToSubstrate()
     return await (this.substrate as Wallet).relinquishCertificate(args, this.originator)
   }
 
   async discoverByIdentityKey(args: { identityKey: PubKeyHex, limit?: PositiveIntegerDefault10Max10000, offset?: PositiveIntegerOrZero }): Promise<DiscoverCertificatesResult> {
-    if (typeof this.substrate !== 'object') {
-      await this.connectToSubstrate()
-    }
+    await this.connectToSubstrate()
     return await (this.substrate as Wallet).discoverByIdentityKey(args, this.originator)
   }
 
   async discoverByAttributes(args: { attributes: Record<CertificateFieldNameUnder50Bytes, string>, limit?: PositiveIntegerDefault10Max10000, offset?: PositiveIntegerOrZero }): Promise<DiscoverCertificatesResult> {
-    if (typeof this.substrate !== 'object') {
-      await this.connectToSubstrate()
-    }
+    await this.connectToSubstrate()
     return await (this.substrate as Wallet).discoverByAttributes(args, this.originator)
   }
 
   async isAuthenticated(args: {} = {}): Promise<{ authenticated: boolean }> {
-    if (typeof this.substrate !== 'object') {
-      await this.connectToSubstrate()
-    }
+    await this.connectToSubstrate()
     return await (this.substrate as Wallet).isAuthenticated(args, this.originator)
   }
 
   async waitForAuthentication(args: {} = {}): Promise<{ authenticated: true }> {
-    if (typeof this.substrate !== 'object') {
-      await this.connectToSubstrate()
-    }
+    await this.connectToSubstrate()
     return await (this.substrate as Wallet).waitForAuthentication(args, this.originator)
   }
 
   async getHeight(args: {} = {}): Promise<{ height: PositiveInteger }> {
-    if (typeof this.substrate !== 'object') {
-      await this.connectToSubstrate()
-    }
+    await this.connectToSubstrate()
     return await (this.substrate as Wallet).getHeight(args, this.originator)
   }
 
   async getHeaderForHeight(args: { height: PositiveInteger }): Promise<{ header: HexString }> {
-    if (typeof this.substrate !== 'object') {
-      await this.connectToSubstrate()
-    }
+    await this.connectToSubstrate()
     return await (this.substrate as Wallet).getHeaderForHeight(args, this.originator)
   }
 
   async getNetwork(args: {} = {}): Promise<{ network: 'mainnet' | 'testnet' }> {
-    if (typeof this.substrate !== 'object') {
-      await this.connectToSubstrate()
-    }
+    await this.connectToSubstrate()
     return await (this.substrate as Wallet).getNetwork(args, this.originator)
   }
 
   async getVersion(args: {} = {}): Promise<{ version: VersionString7To30Bytes }> {
-    if (typeof this.substrate !== 'object') {
-      await this.connectToSubstrate()
-    }
+    await this.connectToSubstrate()
     return await (this.substrate as Wallet).getVersion(args, this.originator)
   }
 }
