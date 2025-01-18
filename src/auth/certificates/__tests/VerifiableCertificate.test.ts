22eae3bb69fad8fa10abfcc3cc7e8907ebc3bc99
Tone Engel <tone@kizmet.org>
2025-01-17 21:36:14 -0500

    CompletedProtoWallet

--- src/auth/certificates/__tests/VerifiableCertificate.test.ts
+++ src/auth/certificates/__tests/VerifiableCertificate.test.ts
@@ -4,7 +4,7 @@
  */
 
 import { VerifiableCertificate } from '../VerifiableCertificate'
-import { ProtoWallet, PrivateKey, SymmetricKey, Utils } from '../../../../mod'
+import { ProtoWallet, PrivateKey, SymmetricKey, Utils, Wallet, AbortActionArgs, AbortActionResult, AcquireCertificateArgs, AcquireCertificateResult, CreateActionArgs, CreateActionResult, DiscoverByAttributesArgs, DiscoverByIdentityKeyArgs, DiscoverCertificatesResult, GetHeaderArgs, GetHeaderResult, GetHeightResult, InternalizeActionArgs, InternalizeActionResult, ListActionsArgs, ListActionsResult, ListCertificatesArgs, ListCertificatesResult, ListOutputsArgs, ListOutputsResult, OriginatorDomainNameStringUnder250Bytes, ProveCertificateArgs, ProveCertificateResult, RelinquishCertificateArgs, RelinquishCertificateResult, RelinquishOutputArgs, RelinquishOutputResult, SignActionArgs, SignActionResult } from '../../../../mod'
 
 describe('VerifiableCertificate', () => {
   const subjectPrivateKey = PrivateKey.fromRandom()
@@ -14,8 +14,8 @@ describe('VerifiableCertificate', () => {
   const verifierPrivateKey = PrivateKey.fromRandom()
   const verifierPubKey = verifierPrivateKey.toPublicKey().toString()
 
-  const subjectWallet = new ProtoWallet(subjectPrivateKey)
-  const verifierWallet = new ProtoWallet(verifierPrivateKey)
+  const subjectWallet = new CompletedProtoWallet(subjectPrivateKey)
+  const verifierWallet = new CompletedProtoWallet(verifierPrivateKey)
 
   const sampleType = Utils.toBase64(new Array(32).fill(1))
   const sampleSerialNumber = Utils.toBase64(new Array(32).fill(2))
@@ -84,7 +84,7 @@ describe('VerifiableCertificate', () => {
 
     it('should fail if the verifier wallet does not have the correct private key (wrong key)', async () => {
       const wrongPrivateKey = PrivateKey.fromRandom()
-      const wrongWallet = new ProtoWallet(wrongPrivateKey)
+      const wrongWallet = new CompletedProtoWallet(wrongPrivateKey)
 
       await expect(verifiableCert.decryptFields(wrongWallet)).rejects.toThrow(
         /Failed to decrypt selectively revealed certificate fields using keyring/
@@ -119,3 +119,66 @@ describe('VerifiableCertificate', () => {
     })
   })
 })
+
+class CompletedProtoWallet extends ProtoWallet implements Wallet {
+  async createAction(args: CreateActionArgs, originator?: OriginatorDomainNameStringUnder250Bytes)
+  : Promise<CreateActionResult> {
+    throw new Error("not implemented")
+  }
+  async signAction(args: SignActionArgs, originator?: OriginatorDomainNameStringUnder250Bytes)
+  : Promise<SignActionResult> {
+    throw new Error("not implemented")
+  }
+  async abortAction(args: AbortActionArgs, originator?: OriginatorDomainNameStringUnder250Bytes)
+  : Promise<AbortActionResult> {
+    throw new Error("not implemented")
+  }
+  async listActions(args: ListActionsArgs, originator?: OriginatorDomainNameStringUnder250Bytes)
+  : Promise<ListActionsResult> {
+    throw new Error("not implemented")
+  }
+  async internalizeAction(args: InternalizeActionArgs, originator?: OriginatorDomainNameStringUnder250Bytes)
+  : Promise<InternalizeActionResult> {
+    throw new Error("not implemented")
+  }
+  async listOutputs(args: ListOutputsArgs, originator?: OriginatorDomainNameStringUnder250Bytes)
+  : Promise<ListOutputsResult> {
+    throw new Error("not implemented")
+  }
+  async relinquishOutput(args: RelinquishOutputArgs, originator?: OriginatorDomainNameStringUnder250Bytes)
+  : Promise<RelinquishOutputResult> {
+    throw new Error("not implemented")
+  }
+  async acquireCertificate(args: AcquireCertificateArgs, originator?: OriginatorDomainNameStringUnder250Bytes)
+  : Promise<AcquireCertificateResult> {
+    throw new Error("not implemented")
+  }
+  async listCertificates(args: ListCertificatesArgs, originator?: OriginatorDomainNameStringUnder250Bytes)
+  : Promise<ListCertificatesResult> {
+    throw new Error("not implemented")
+  }
+  async proveCertificate(args: ProveCertificateArgs, originator?: OriginatorDomainNameStringUnder250Bytes)
+  : Promise<ProveCertificateResult> {
+    throw new Error("not implemented")
+  }
+  async relinquishCertificate(args: RelinquishCertificateArgs, originator?: OriginatorDomainNameStringUnder250Bytes)
+  : Promise<RelinquishCertificateResult> {
+    throw new Error("not implemented")
+  }
+  async discoverByIdentityKey(args: DiscoverByIdentityKeyArgs, originator?: OriginatorDomainNameStringUnder250Bytes)
+  : Promise<DiscoverCertificatesResult> {
+    throw new Error("not implemented")
+  }
+  async discoverByAttributes(args: DiscoverByAttributesArgs, originator?: OriginatorDomainNameStringUnder250Bytes)
+  : Promise<DiscoverCertificatesResult> {
+    throw new Error("not implemented")
+  }
+  async getHeight(args: {}, originator?: OriginatorDomainNameStringUnder250Bytes)
+  : Promise<GetHeightResult> {
+    throw new Error("not implemented")
+  }
+  async getHeaderForHeight(args: GetHeaderArgs, originator?: OriginatorDomainNameStringUnder250Bytes)
+  : Promise<GetHeaderResult> {
+    throw new Error("not implemented")
+  }
+}
