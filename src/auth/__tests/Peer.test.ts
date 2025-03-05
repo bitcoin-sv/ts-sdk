a1dd6c8b117623041736d6b90f593e35d08c8fd8
Ty J Everett <ty@tyweb.us>
2025-03-05 10:25:06 -0800

    Time out

--- src/auth/__tests/Peer.test.ts
+++ src/auth/__tests/Peer.test.ts
@@ -296,7 +296,7 @@ describe('Peer class mutual authentication and certificate exchange', () => {
     await aliceFirstDevice.toPeer(Utils.toArray('Back on my first device now, Bob! Can you still hear me?'))
     await new Promise(resolve => setTimeout(resolve, 2000))
     expect(alice1MessageHandler.mock.calls.length).toEqual(2)
-  }, 15000)
+  }, 30000)
 
   it('Bob requests certificates from Alice, Alice does not request any from Bob', async () => {
     const alicePubKey = (await walletA.getPublicKey({ identityKey: true }))
